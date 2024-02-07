f26047d8dbe49443f4c946087fe0cb510caa9a11
sherbek turdiyev <sherbekturdiyev98@gmail.com>
2024-02-07 23:50:13 +0500

    draw fix

--- indexTwo.js
+++ indexTwo.js
@@ -8,12 +8,10 @@ canvasTwo.width = window_width;
 canvasTwo.style.background = '#94ef81';
 
 let startTime;
+let img = new Image();
+img.src = '../canvas test/pic/icons8-car-48.png';
 let drawSquare = function (x, y, size) {
     ctx.clearRect(0, 0, canvasTwo.width, canvasTwo.height);
-    ctx.lineWidth = 1;
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(x, y, size, size);
-    ctx.strokeRect(x, y, size, size);
 
     ctx.beginPath();
     ctx.lineWidth = 1;
@@ -27,6 +25,8 @@ let drawSquare = function (x, y, size) {
     ctx.lineTo(470, 310);
     ctx.closePath()
     ctx.stroke();
+
+    ctx.drawImage(img, x, y, 20, 20);
 };
 
 function animateSquare(timestamp) {
@@ -35,26 +35,39 @@ function animateSquare(timestamp) {
     let newX, newY;
 
     if (elapsedTime < 4000) {
-        newX = 465;
-        newY = 310 - Math.min(elapsedTime / 940) * 50;
+        newX = 460;
+        newY = 305 - Math.min(elapsedTime / 940) * 50;
 
     } else if(elapsedTime >= 4000){
         const rotationTime = elapsedTime - 4000;
         const rotationAngle = (rotationTime / 2000) * Math.PI;
-        const centerX = 475;
-        const centerY = 95;
+        const centerX = 470;
+        const centerY = 90;
         const radius = 10;
         newX = centerX - Math.cos(rotationAngle) * radius;
         newY = centerY - Math.sin(rotationAngle) * radius;
 
-    }else if (elapsedTime >= 6000) { // o'xshamadi
-        newX = 485;
-        newY = 95 + Math.min((elapsedTime - 6000) / 4000, 1) * 50;
+    }
+    if(elapsedTime >= 6000){
+        newX = 480;
+        newY = 90 + Math.min((elapsedTime - 6000) / 4000, 1) * 200;
+    }
+    if(elapsedTime >= 10000){
+        const rotationTime = elapsedTime - 10000;
+        const rotationAngle = (rotationTime / 2000) * Math.PI;
+        const centerX = 490;
+        const centerY = 290;
+        const radius = 10;
+        newX = centerX - Math.cos(rotationAngle) * radius;
+        newY = centerY + Math.sin(rotationAngle) * radius;
+    }
+    if (elapsedTime >=12000){
+        newX = 500;
+        newY = 290 - Math.min((elapsedTime - 12000) / 3000, 1) * 210;
     }
 
     drawSquare(newX, newY, 10);
-    if (elapsedTime < 10000) {
-        console.log(newX, newY)
+    if (elapsedTime < 16000) {
         requestAnimationFrame(animateSquare);
     }
 }
